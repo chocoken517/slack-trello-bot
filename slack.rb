6e949ef98b15c94e449886c4d0425f0bfb4db6a9
chocoken517 <ken57.gm@gmail.com>
2016-06-07 11:31:49 +0900

    token bot name change

--- slack.rb
+++ slack.rb
@@ -3,36 +3,34 @@ Dotenv.load
 require './trello_connection'
 require 'slack-ruby-client'
 
-fail 'Missing ENV[SLACK_API_TOKENS]!' unless ENV.key?('SLACK_API_TOKENS')
+fail 'Missing ENV[SLACK_API_TOKEN]!' unless ENV.key?('SLACK_API_TOKEN')
 
 $stdout.sync = true
 logger = Logger.new($stdout)
 logger.level = Logger::DEBUG
 
-ENV['SLACK_API_TOKENS'].split.each do |token|
-  logger.info "Starting #{token[0..12]} ..."
+logger.info 'Starting ...'
 
-  client = Slack::RealTime::Client.new(token: token)
+client = Slack::RealTime::Client.new(token: ENV['SLACK_API_TOKEN'])
 
-  client.on :hello do
-    logger.info "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
-  end
-
-  client.on :message do |data|
-    logger.info data
-
-    client.typing channel: data.channel
-
-    case data.text
-    when ENV['TRELLO_BOARD'] + ' hi' then
-      client.message channel: data.channel, text: "Hi <@#{data.user}>!"
-    when /^#{ENV['TRELLO_BOARD']}/ then
-      client.message channel: data.channel, text: "Sorry <@#{data.user}>, what?"
-    end
-  end
-
-  client.start_async
+client.on :hello do
+  logger.info "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
 end
+
+client.on :message do |data|
+  logger.info data
+
+  client.typing channel: data.channel
+
+  case data.text
+  when ENV['SLACK_BOT_NAME'] + ' hi' then
+    client.message channel: data.channel, text: "Hi <@#{data.user}>!"
+  when /^#{ENV['SLACK_BOT_NAME']}/ then
+    client.message channel: data.channel, text: "Sorry <@#{data.user}>, what?"
+  end
+end
+
+client.start_async
 
 loop do
   Thread.pass
